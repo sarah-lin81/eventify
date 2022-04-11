ec47e07fd93b385f7a7a8a32bf068181f5f8d449
sarah <sarahlin280@gmail.com>
2022-04-11 17:53:22 +0800

    add google_oauth2 provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_one_attached :avatar, dependent: :destroy
 
@@ -12,5 +13,14 @@ class User < ApplicationRecord
 
   def name
     email.split("@")[0]
+  end
+
+  def self.create_from_provider_data(provider_data)
+    where(email: provider_data.info.email).first_or_create do |user|
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.provider = provider_data.provider
+      user.uid = provider_data.uid
+    end
   end
 end
