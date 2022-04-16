76ca13cef2af8f9d68692099617754c7baf0b410
sarah <sarahlin280@gmail.com>
2022-04-16 23:15:57 +0800

    add like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_12_045022) do
+ActiveRecord::Schema.define(version: 2022_04_16_150653) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,15 @@ ActiveRecord::Schema.define(version: 2022_04_12_045022) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_likes_on_event_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -104,4 +113,6 @@ ActiveRecord::Schema.define(version: 2022_04_12_045022) do
   add_foreign_key "enrollments", "events"
   add_foreign_key "enrollments", "users"
   add_foreign_key "events", "users"
+  add_foreign_key "likes", "events"
+  add_foreign_key "likes", "users"
 end
